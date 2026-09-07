6b695b4c54125789b40c7de7bd31547b58dd6ae5
Ash Bean <ashleybean2603@gmail.com>
2026-09-07 10:46:46 -0500

    URL

---
+++ Juego_Reyes_Calville/src/AbrirGithub.ts
@@ -0,0 +1,18 @@
+import * as ecs from '@8thwall/ecs'
+
+ecs.registerComponent({
+  name: 'InstagramButton',
+
+  stateMachine: ({eid}) => {
+    ecs.defineState('default')
+      .initial()
+      .listen(eid, ecs.input.UI_CLICK, () => {
+
+        console.log('BUTTON CLICKED')
+
+        window.location.href =
+          'https://github.com/ashcabeear/Juego-Deploy'
+
+      })
+  },
+})
